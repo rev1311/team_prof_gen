a02fc499c04ed8cf30f268f9f9872472f529eb51
Michael Partin <unmykx@gmmail.com>
2020-01-08 19:37:53 -0800

    added employee class

---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {
+
+    }
+    getId() {
+
+    }
+    getEmail() {
+
+    }
+    getRole() {
+        
+    }
+}
